316a7b6363f7f9da3da221aa2049417fc5f82f8d
Andrew Penkrat <contact.aldrog@gmail.com>
2017-04-12 22:26:23 +0300

    Link qt5 on OS X

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,6 +2,7 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install qt5
+    brew link qt5 --force
 else
     sudo apt-get -y install qt58base qt58declarative qt58quickcontrols qt58quickcontrols2 qt58svg qt58graphicaleffects
 fi
