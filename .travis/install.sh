86469cde457031784ad055743c8755dfb96ab42c
Andrew Penkrat <contact.aldrog@gmail.com>
2017-04-12 22:03:50 +0300

    Install QQC1 for Dialogs

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,5 +3,5 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install qt5
 else
-    sudo apt-get -y install qt58base qt58declarative qt58quickcontrols2 qt58svg qt58graphicaleffects
+    sudo apt-get -y install qt58base qt58declarative qt58quickcontrols qt58quickcontrols2 qt58svg qt58graphicaleffects
 fi
