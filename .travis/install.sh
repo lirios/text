e6a9396594b17ba2daa755167cf9841419edd784
Andrew Penkrat <contact.aldrog@gmail.com>
2017-04-12 21:32:36 +0300

    Source environment on top level

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,5 +4,4 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install qt5
 else
     sudo apt-get -y install qt58base qt58declarative qt58quickcontrols2 qt58svg qt58graphicaleffects
-    source /opt/qt58/bin/qt58-env.sh
 fi
