f6e892f0d740199a8f9554238093af89d3c29950
Andrew Penkrat <contact.aldrog@gmail.com>
2017-04-21 20:37:48 +0300

    Minor improvements to Travis configuration Deploy to bintray on tag pushes

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,4 +5,5 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew link qt5 --force
 else
     sudo apt-get -y install qt58base qt58declarative qt58quickcontrols qt58quickcontrols2 qt58svg qt58graphicaleffects
+    source /opt/qt58/bin/qt58-env.sh
 fi
