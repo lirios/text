b88d0d71f6b34c34f63dd280ee26628ce8867678
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-03-03 13:07:49 +0100

    Build with Docker on Linux
    Use KDE Neon container to perform Linux builds.
    This way we no longer have to worry about old libraries.

--- .travis/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    brew update
-else
-    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-    sudo add-apt-repository -y ppa:beineri/opt-qt58-trusty
-    sudo apt-get update -qq
-fi
