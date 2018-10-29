96eb596ac6173e5f7dd3e240541fbb470afe42ee
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-10-29 22:34:14 +0100

    Port build system to CMake
    Qbs is now deprecated:
    http://blog.qt.io/blog/2018/10/29/deprecation-of-qbs/
    http://lists.qt-project.org/pipermail/development/2018-October/034023.html
    
    The only viable alternative to qmake left is CMake.
    Chances are that it will be used to build Qt 6, and in that
    case QtCreator support will hopefully be improved.
    
    CMake is also widely adopted and thanks to the server mode
    it is easier to use with QtCreator nowadays, although some
    basic features such as adding files from the IDE are not
    supported yet.  Not to mention missing Android integration.
    
    Despite Qbs being a clearly superior build tool than CMake,
    we are forced to abandon it in favor of the status quo.
    
    Even though Qbs is still alive and some companies appear
    to be willing to at least maintain QtCreator integration
    in its current status, we are not guaranteed that the
    build system will evolve with all the promised feature.
    
    So thanks to the Qbs team for letting us dream, but we
    can't risk Liri health for a build system that will
    potentially be stale.
    
    The official build system is now CMake.
    
    Qbs files will probably stay here for a while, but we will
    not update them anymore.  If, at some point, Qbs will get
    momentum we will evaluate the situation again.
    
    Issue: #46

---
+++ .travis/build-linux.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+curl "https://raw.githubusercontent.com/lirios/infra-travis/master/installer" | bash
+
+source /usr/local/share/liri-travis/functions
+
+# Install artifacts
+travis_start "artifacts"
+msg "Install artifacts..."
+for name in cmakeshared fluid; do
+    /usr/local/bin/liri-download-artifacts $TRAVIS_BRANCH ${name}-artifacts.tar.gz
+done
+travis_end "artifacts"
+
+# Configure
+travis_start "configure"
+msg "Setup CMake..."
+mkdir build
+cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DINSTALL_LIBDIR=/usr/lib64 \
+    -DINSTALL_QMLDIR=/usr/lib64/qt5/qml \
+    -DINSTALL_PLUGINSDIR=/usr/lib64/qt5/plugins
+travis_end "configure"
+
+# Build
+travis_start "build"
+msg "Build..."
+make -j $(nproc)
+travis_end "build"
+
+# Install
+travis_start "install"
+msg "Install..."
+make install
+travis_end "install"
+
+# Package
+#travis_start "appimage"
+#msg "Create AppImage..."
+#/usr/local/bin/liri-build-appimage io.liri.Text
+#mkdir -p travis-out
+#mv Liri_Text*.AppImage travis-out/Liri_Text-git-$(date +%Y%m%d-%H%M%S)-$(git rev-parse --short HEAD)-x86_64.AppImage
+#travis_end "appimage"
