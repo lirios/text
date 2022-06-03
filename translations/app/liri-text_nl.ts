d1800b0683a9b703e117f0fd3e10eb751a7ef9a2
Liri CI <ci@liri.io>
2022-06-03 00:38:13 +0000

    Automatic merge of Transifex translations
    [ci skip]

--- translations/app/liri-text_nl.ts
+++ translations/app/liri-text_nl.ts
@@ -4,87 +4,87 @@
     <message>
         <location filename="../../src/qml/EditPage.qml" line="71"/>
         <source>New Document</source>
-        <translation>Nieuw Document</translation>
+        <translation>Nieuw bestand</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="78"/>
         <source>Save</source>
-        <translation>Bewaar</translation>
+        <translation>Opslaan</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="86"/>
         <source>Find</source>
-        <translation>Vind</translation>
+        <translation>Zoeken</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="94"/>
         <source>Save As</source>
-        <translation>Opslaan Als</translation>
+        <translation>Opslaan als</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="102"/>
         <source>Close</source>
-        <translation>Sluit</translation>
+        <translation>Sluiten</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="224"/>
         <source>You have unsaved changes. Do you want to save them before closing the file?</source>
-        <translation>Je hebt onopgeslagen wijzigingen. Wil je ze opslaan voor je het bestand aflsuit?</translation>
+        <translation>Het bestand bevat niet-opgeslagen wijzigingen. Wil je ze opslaan voor je het bestand sluit?</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="261"/>
         <source>The file was changed from outside. Do you wish to reload its content?</source>
-        <translation>Het bestand is bewerkt van buitenaf. Wil je de inhoud herladen?</translation>
+        <translation>Het bestand is elders bewerkt. Wil je de inhoud herladen?</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="278"/>
         <source>Reload</source>
-        <translation>Herlaad</translation>
+        <translation>Herladen</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="284"/>
         <source>Ignore</source>
-        <translation>Negeer</translation>
+        <translation>Negeren</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="336"/>
         <source>Undo</source>
-        <translation>Ongedaan Maken</translation>
+        <translation>Ongedaan maken</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="341"/>
         <source>Redo</source>
-        <translation>Overdoen</translation>
+        <translation>Opnieuw</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="349"/>
         <source>Copy</source>
-        <translation>Kopieer</translation>
+        <translation>Kopiëren</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="354"/>
         <source>Cut</source>
-        <translation>Knip</translation>
+        <translation>Knippen</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="359"/>
         <source>Paste</source>
-        <translation>Plak</translation>
+        <translation>Plakken</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="364"/>
         <source>Delete</source>
-        <translation>Verwijder</translation>
+        <translation>Verwijderen</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="372"/>
         <source>Select All</source>
-        <translation>Selecteer Alle</translation>
+        <translation>Alles selecteren</translation>
     </message>
     <message>
         <location filename="../../src/qml/EditPage.qml" line="396"/>
         <source>File operation error</source>
-        <translation>Bestands opdracht fout</translation>
+        <translation>Bestandsafhandelingsfout</translation>
     </message>
 </context>
 <context>
@@ -92,17 +92,17 @@
     <message>
         <location filename="../../src/qml/RecentFilesPage.qml" line="31"/>
         <source>Recent Files</source>
-        <translation>Recente Bestanden</translation>
+        <translation>Onlangs geopend</translation>
     </message>
     <message>
         <location filename="../../src/qml/RecentFilesPage.qml" line="36"/>
         <source>Open</source>
-        <translation>Open</translation>
+        <translation>Openen</translation>
     </message>
     <message>
         <location filename="../../src/qml/RecentFilesPage.qml" line="60"/>
         <source>You don&apos;t have recently open files</source>
-        <translation>Je hebt geen recent geopende bestanden</translation>
+        <translation>Er zijn geen onlangs geopende bestanden</translation>
     </message>
 </context>
 <context>
@@ -110,12 +110,12 @@
     <message>
         <location filename="../../src/main.cpp" line="79"/>
         <source>Advanced text editor made in accordance with Material Design.</source>
-        <translation>Geavanceerde tekstverwerker gemaakt volgens Material Design.</translation>
+        <translation>Een uitgebreide tekstbewerker, gemaakt in de Material Design-taal.</translation>
     </message>
     <message>
         <location filename="../../src/main.cpp" line="83"/>
         <source>Start the editor with a new document.</source>
-        <translation>Start de bewerker met een nieuw document.</translation>
+        <translation>Open de bewerker met een nieuw bestand.</translation>
     </message>
     <message>
         <location filename="../../src/main.cpp" line="85"/>
@@ -125,7 +125,7 @@
     <message>
         <location filename="../../src/main.cpp" line="86"/>
         <source>Path to a file to open for editing.</source>
-        <translation>Pad naar een bestand om te openen voor bewerking.</translation>
+        <translation>De locatie van het te bewerken bestand.</translation>
     </message>
 </context>
 </TS>
